346dcecc47c151b375189c4c1aa72f30eabccac7
James Kearney <jameskea@seas.upenn.edu>
2023-04-25 14:40:19 -0400

    can now switch between calendar accounts

--- calendux.sh
+++ calendux.sh
@@ -12,71 +12,119 @@ function add_event() {
   printf "The event was added\n"
 }
 
+function search_events() {
+  printf "Type in an event to search\n"
+  read search
+  gcalcli search "$search"
+}
+
+function search_agenda() {
+  printf "Type in a start day for the agenda\n"
+  read start
+  printf "Type in a end day for the agenda\n"
+  read end
+  gcalcli agenda $start $end
+}
+
 function edit_event() {
   printf "Which event would you like to edit \n"
-  printf "[1] Show Events of this month\n"
   read event
   if [ ! -z "$event" ]; then
-    case "$option" in
-      1)
-        gcalcli agenda 1 30
-    esac
     printf "loading... \n" 
     gcalcli edit $event
   fi
 }
 
+#Shows agenda for the current week
 function show_week_agenda() {
   printf "loading... \n"
   gcalcli agenda
 }
 
+#Shows calendar view of current week
 function show_week_calendar() {
   printf "loading... \n"
   gcalcli calw
 }
 
+#Shows agenda for the current month
 function show_month_agenda() {
   printf "loading... \n"
   gcalcli agenda 1 30
 }
 
+function switch_accounts() {
+  printf "enter client_id: \n"
+  read client_id
+  printf "enter client_secret: \n"
+  read client_secret
+  gcalcli --client-id=$client_id.apps.googleusercontent.com --client-secret=$client_secret list
+}
+
+#Shows calendar view of current month
 function show_month_calendar() {
   printf "loading... \n"
   gcalcli calm
 }
 
+#Shows agenda for current day
+function show_day_agenda() {
+  printf "loading... \n"
+  gcalcli agenda today
+}
+
+#Prints menu screen for user to pick option
 function print_main_menu() {
-  printf "Welcome to the Calendux James!\n"
+  printf "Welcome to the Calendux!\n"
   printf "What would you like to do?\n"
-  printf "[1] Show Week Agenda\n"
-  printf "[2] Show Month Agenda\n"
-  printf "[3] Show Week Calendar\n"
-  printf "[4] Show Month Calendar\n"
-  printf "[5] Add Event\n"
-  printf "[6] Edit Event\n"
+  printf "[1] Show Day Agenda\n"
+  printf "[2] Show Week Agenda\n"
+  printf "[3] Show Month Agenda\n"
+  printf "[4] Show Week Calendar\n"
+  printf "[5] Show Month Calendar\n"
+  printf "[6] Add Event\n"
+  printf "[7] Edit Event\n"
+  printf "[8] Search Event\n"
+  printf "[9] Search Agenda\n"
+  printf "[10] Switch Accounts\n"
   printf "[<Enter> or CTRL+D] Quit\n"
 }
 
+#Main function that is running and recieving user prompts
 function main_menu() {
   print_main_menu
   read option
   while [ ! -z "$option" ]; do
     case "$option" in
       1)
-        show_agenda
+        show_day_agenda
         ;;
       2)
-        add_event
+        show_week_agenda
         ;;
       3)
-        edit_event
+        show_month_agenda
         ;;
       4)
-        show_week
+        show_week_calendar
         ;;
       5)
-        gcalcli calm
+        show_month_calendar
+        ;;
+      6)
+        add_event
+        ;;
+      7)
+        edit_event
+        ;;
+      8)
+        search_events
+        ;;
+      9)
+        search_agenda
+        ;;
+      10)
+        switch_accounts
         ;;
       *)
         printf "Invalid option.\n"
